313968dbc7e94c4f1a7549ece09de5ea346da11c
Redbruno7 <brunorodgers7@gmail.com>
2024-11-11 16:32:13 -0300

    Hello World

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+# Primeiro Programa With Momozi s2s2
+# Data: 11/11/2024
+
+import os
+import random
+
+
+os.system('cls')
+
+# Título
+print('=' * 70)
+print(f'HELLO WORLD, FROM LADO-B!')
+print('=' * 70)
+
+# Entrada variável
+print()
+print('-' * 70)
+message = input('Tell us anything: ')
+print('-' * 70)
