53d2bfdc0c8d833efc548940c3bcd4e8347e506a
Redbruno7 <brunorodgers7@gmail.com>
2024-11-12 12:20:54 -0300

    About us and manutencions

---
+++ Introduction/about_us.py
@@ -0,0 +1,33 @@
+# About us using variant - Training
+# Data: 11/11/2024
+
+import os
+import random
+
+
+os.system('cls')
+
+# Título
+print('=' * 80)
+print(f'ABOUT US')
+print('=' * 80)
+
+# Entrada variável
+name = 'Bruno C. Rodgers'
+birth = 1999
+study_area = 'Python'
+introduction_message = (f'Hello! Welcome to our portfolio! '
+f'It will be a plesure to show our evolution!')
+
+# Exit
+print()
+print('=' * 80)
+print('MEMBERS')
+print('=' * 80)
+print(f'Name: {name}')
+print(f'Birth: {birth}')
+print(f'Study Areas: {study_area}')
+print('-' * 80)
+print(f'{introduction_message}')
+print('-' * 80)
+print('=' * 80)
--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,5 @@
-# Primeiro Programa With Momozi s2s2
-# Data: 11/11/2024
+# First Program With Momozi s2s2
+# Date: 11/11/2024
 
 import os
 import random
@@ -7,14 +7,13 @@ import random
 
 os.system('cls')
 
-# Título
+# Title
 print('=' * 70)
 print(f'HELLO WORLD, FROM LADO-B!')
 print('=' * 70)
 
-# Entrada variável
+# Input variant
 print()
 print('-' * 70)
 message = input('Tell us anything: ')
 print('-' * 70)
-teste
