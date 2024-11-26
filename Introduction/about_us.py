33cfff46e0c5f2050d4f688e651f70924bac7bcb
Redbruno7 <brunorodgers7@gmail.com>
2024-11-26 15:54:02 -0300

    Lado B Projeto

--- Introduction/about_us.py
+++ Introduction/about_us.py
@@ -1,8 +1,7 @@
-# About us using variant - Training
-# Data: 11/11/2024
+# About us
+# Date: 11/11/2024
 
 import os
-import random
 
 
 os.system('cls')
@@ -16,8 +15,6 @@ print('=' * 80)
 name = 'Bruno C. Rodgers'
 birth = 1999
 study_area = 'Python'
-introduction_message = (f'Hello! Welcome to our portfolio! '
-f'It will be a plesure to show our evolution!')
 
 # Exit
 print()
@@ -26,9 +23,10 @@ print('MEMBERS')
 print('=' * 80)
 print(f'Name: {name}')
 print(f'Birth: {birth}')
-print(f'Study Areas: {study_area}')
+print(f'Study Area: {study_area}')
 print('-' * 80)
-print(f'{introduction_message}')
+print('Hello! This is our e-Commerce development project')
+print('It will be a plesure to show our evolution!')
 print('-' * 80)
 
 print('=' * 80)
---
+++ Introduction/planning.py
@@ -0,0 +1,30 @@
+# PLANEJAMENTO DO PROJETO
+# Data: 26/11/2024
+
+# INTRODUÇÃO
+# 1 - Explicação do projeto
+# 2 - Aplicação real do projeto
+
+# DESENVOLVIMENTO INICIAL
+# 1 - Definir um nicho
+# 2 - Definir clientela / Persona do cliente ideal
+# 3 - Diferenciação da concorrência
+
+# DESENVOLVIMENTO PROGRAMÁTICO
+# 1 - Definir estrutura técnica / linguagens de programação utilizadas
+# 2 - Criar fluxogramas para funcionalidades do sistema
+# 3 - Desenvolver funcionalidades principais (Login, produtos, carrinho, pagamento, etc)
+# 4 - Projetar design responsivo
+# 5 - Deselvolver protótipos para testes
+
+# CONCLUSÃO
+# 1 - Destacar evolução contínua do projeto / Alcance de evolução
+# 2 - Citar aprendizados no desenvolvimento
+
+# IMPLEMENTAÇÃO
+# 1 - Adicionar um README no GitHub explicando o projeto
+# 2 - Hospedar site em plataforma
+# 3 - Monetizar negócio / Exemplos:
+#   a - Venda de produtos próprios
+#   b - Dropshipping / Parceria com fornecedores
+#   c - Promoção de produtos terceirizados para ganho em comissão
